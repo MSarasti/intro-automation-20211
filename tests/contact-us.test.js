e99b7b57bdaac47cbc322eb6483ec3209cb0a1d4
MSarasti <54419255+MSarasti@users.noreply.github.com>
2021-11-09 10:57:02 -0500

    Update contact-us.test.js

--- tests/contact-us.test.js
+++ tests/contact-us.test.js
@@ -9,14 +9,14 @@ test('The contact us option is successfully', async (t) => {
     const message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor commodo ex a ultrices. Suspendisse quis vulputate ante. Aenean maximus."
 
     // Act
-    await t.click("")
-    await t.click("")
-    await t.click(Selector("").withText(subjectHeading))
-    await t.typeText("", emailAdress)
-    await t.typeText("", message)
-    await t.click("")
+    await t.click("#contact-link")
+    await t.click("#uniform-id_contact.selector")
+    await t.click(Selector("#id_contact").find("option").withText(subjectHeading))
+    await t.typeText("#email", emailAdress)
+    await t.typeText("#message", message)
+    await t.click("#submitMessage")
 
     // Assert
+    // await t.expect(Selector("#center_column ol").textContent).eql("Error.")
     await t.expect(Selector("#center_column p").textContent).eql("Your message has been successfully sent to our team.")
-
 });
